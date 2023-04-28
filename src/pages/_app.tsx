5e215f1950987be1ae43db3b699c61ae5e227696
Eidmone Tagaca <etagaca@csub.edu>
2023-04-27 21:00:06 -0700

    Add page transition loader

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,13 @@
+import AppContextProvider from '@/components/AppContextProvider';
+import NextNProgress from 'nextjs-progressbar';
+import { SessionProvider } from 'next-auth/react';
 import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
-import { SessionProvider } from 'next-auth/react';
-import AppContextProvider from '@/components/AppContextProvider';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <AppContextProvider>
+      <NextNProgress color="#15c39a" />
       <SessionProvider session={pageProps.session}>
         <Component {...pageProps} />
       </SessionProvider>
